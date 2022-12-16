ab623c572f562ec6ca2d3807a9f03d71819f875b
masachika <masachika.kmd@gmail.com>
2022-12-16 10:37:22 +0900

    fix

--- day1/big_countries/a.sql
+++ day1/big_countries/a.sql
@@ -1,2 +1,2 @@
 use big_countries;
-select name, population, area from World where area > 3000000 or population > 25000000;
+select name, population, area from World where area > 3000000 or population >= 25000000;
